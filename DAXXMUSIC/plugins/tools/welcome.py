f9095aa8c81e5113f4d37a344c89daf18effa9d0
Visionx-x <telegrambott19@gmail.com>
2024-10-05 14:35:55 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -52,11 +52,11 @@ from DAXXMUSIC.utils.database import get_assistant, is_active_chat
 
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://graph.org/file/462ea653de0a313f8302a.jpg",
+    "https://graph.org/file/462ea653de0a313f8302a.jpg",
+    "https://graph.org/file/462ea653de0a313f8302a.jpg",
+    "https://graph.org/file/462ea653de0a313f8302a.jpg",
+    "https://graph.org/file/462ea653de0a313f8302a.jpg",
 ]
 # --------------------------------------------------------------------------------- #
 
