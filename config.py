34fde821689fb0283ad645f04c56281cecdf5080
Ishu_official <somnathhor12052005@gmail.com>
2024-10-07 04:39:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/somushilpa/SOMU_XD",
+    "https://github.com/tutorialwindoxIk/SOMU_V2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
