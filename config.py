7555d24cbe00434c2ada10a1f4011195e3e3205d
Ishu_official <somnathhor12052005@gmail.com>
2024-12-28 21:33:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -69,8 +69,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/somueditingzone")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/somueditingzone")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MeowMusicSupport")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/meowxdmusix")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
