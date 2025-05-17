eaf3be348d5bb65fd4964363a8008137a4f8607a
Ishu_official <somnathhor12052005@gmail.com>
2025-05-17 19:48:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -123,6 +123,37 @@ STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 STRING6 = getenv("STRING_SESSION6", None)
 STRING7 = getenv("STRING_SESSION7", None)
+AMBOT = [
+    "💞",
+    "🔎",
+    "🔍",
+    "🧪",
+    "💣",
+     "⚡️",
+     "🔥",
+     "🕺",
+     "🎩",
+     "🌈",
+     "👀",
+     "🥂",
+     "🍾",
+    "🥃",
+    "🥤",
+    "🍽",
+    "🍭",
+    "🥀",
+    "🦋",
+    "🚓",
+    "🙏🏻",
+    "🚀",
+    "💎",
+    "🔮",
+    "🪄",
+    "💌",
+    "👻",
+    "💤",
+    "🧨"
+]
 BANNED_USERS = filters.user()
 adminlist = {}
 lyrical = {}
